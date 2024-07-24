0769ad9aafff53e25006b7ae6d2c0bc8932b83b5
Sean Tilson <seantilson@gmail.com>
2024-07-24 15:34:19 +0000

    refit final pipes with only important features
    save pipelines

---
+++ src/noteboook_functions.py
@@ -0,0 +1,66 @@
+# Functions from notebooks for easy import into other notebooks
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def find_features(X_train, fitted_pipe, initial_drop):
+    corr_dropped = list(fitted_pipe['corr_selector'].features_to_drop_)
+    auto_dropped = initial_drop + corr_dropped
+    cols = [col for col in X_train.columns if col not in auto_dropped]
+
+    features = fitted_pipe['feat_selection'].get_support()
+    X = X_train.filter(cols)
+    if len(X.columns) != features.shape[0]:
+        raise ValueError
+    feat_selected_dropped = []
+    for index, col in enumerate(X.columns):
+        if not features[index]:
+            X.drop(col, axis=1, inplace=True)
+        else:
+            feat_selected_dropped.append(col)
+    return list(X.columns), auto_dropped + feat_selected_dropped
+
+
+
+def feature_importance_logistic_regression(pipe, step_name, X_TrainSet, initial_drop):
+  coefficients = pipe[step_name].coef_[0]
+  features, _ = find_features(X_TrainSet, pipe, initial_drop)
+  importance_list = [(feature,X_TrainSet[feature].std()*coeff)
+          for feature, coeff in zip(features, coefficients)]
+  df_feature_importance = (pd.DataFrame(data={
+    'Features':[term[0] for term in importance_list],
+    'Importance':[abs(term[1]) for term in importance_list]
+    })
+  .sort_values(by='Importance', ascending=False)
+  )
+
+  best_features = df_feature_importance['Features'].to_list()
+
+  # Most important features statement and plot
+  print(f"* These are the {len(best_features)} most important features in descending order. "
+        f"The model was trained on them: \n{df_feature_importance['Features'].to_list()}")
+
+  df_feature_importance.plot(kind='bar',x='Features',y='Importance')
+
+  for feature, coefficient in zip(features, coefficients):
+    std = X_TrainSet[feature].std()
+    print(feature, std*coefficient)
+
+
+# This code is from Unit 04 of the Scikit-Learn lessons
+def feature_importance_tree_based_models(model, columns):
+  df_feature_importance = (pd.DataFrame(data={
+      'Features': columns,
+      'Importance': model.feature_importances_})
+  .sort_values(by='Importance', ascending=False)
+  )
+
+  best_features = df_feature_importance['Features'].to_list()
+
+  # Most important features statement and plot
+  print(f"* These are the {len(best_features)} most important features in descending order. "
+        f"The model was trained on them: \n{df_feature_importance['Features'].to_list()}")
+
+  df_feature_importance.plot(kind='bar',x='Features',y='Importance')
+  plt.show()
+
