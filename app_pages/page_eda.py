1bb3e5e6eb13c2c1c867593cdacdd4c382165ac2
Sean Tilson <seantilson@gmail.com>
2024-07-31 07:57:48 +0000

    edit notebook 06 and 07
    edit eda page
    
    edit hypothesis 1 page

--- app_pages/page_eda.py
+++ app_pages/page_eda.py
@@ -7,90 +7,117 @@ import seaborn as sns
 from src.utils import get_df
 from src.utils import display_feature_name as disp
 
-sns.set_style('whitegrid')
+sns.set_style("whitegrid")
 
 
 def page_eda_body():
     st.write("## Exploratory Data Analysis")
-    blurb = "We looked at several facets of our dataset.\n"\
-            "* We looked at the distribution of the features. We used the "\
-            "[omnibus test of normality](https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.normaltest.html) "\
-            "to see if any of the features were normally distributed.\n"\
-            "* We looked at the correlation coefficients between different "\
-            "features. We then focused on the correlation coefficients where "\
-            "one feature was the home team.\n"\
-            "* We looked at the Predictive Power score between the different "\
-            "features. We then focused on their relation to our target: home "\
-            "team wins.\n"
+    blurb = (
+        "We looked at several facets of our dataset.\n"
+        "* We looked at the distribution of the features. We used the "
+        "[omnibus test of normality](https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.normaltest.html) "
+        "to see if any of the features were normally distributed.\n"
+        "* We looked at the correlation coefficients between different "
+        "features. We then focused on the correlation coefficients where "
+        "one feature was the home team.\n"
+        "* We looked at the Predictive Power score between the different "
+        "features. We then focused on their relation to our target: home "
+        "team wins.\n"
+    )
     st.write(blurb)
-    data = get_df('game_eda','datasets/clean/csv')
-    corr_df = get_df('eda_spearman_corr','eda')
-    normality_scores = get_df('normality_scores','eda')
-    pps_results = get_df('pps_results','eda')
+    data = get_df("game_eda", "datasets/clean/csv")
+    corr_df = get_df("eda_spearman_corr", "eda")
+    normality_scores = get_df("normality_scores", "eda")
+    pps_results = get_df("pps_results", "eda")
     features = list(corr_df.columns)
-    
+
     st.write("## Feature Distributions")
-    st.write("Select a feature to see what its score on the normality test "\
-             "is.")
+    st.write("Select a feature to see what its score on the normality test " "is.")
     feature = st.selectbox("Feature", features, index=0)
-    score = normality_scores.loc[feature]['W']
-    pval = normality_scores.loc[feature]['pval']
-    st.write(f"{disp(feature)} scored {score} and has a p-value f {pval}. In "\
-             "order to be considered normal, the p-value must be larger than "\
-             "0.05.")
-    st.write("While none of the features were found to be normally "\
-            "distributed, we were able to transform some of them in order to "\
-            "obtain normal distributions.")
-    st.write("Select a feature to plot its distribution for home and away "\
-             "teams.")
-    single_features = [feature.split('_')[0] for feature in features]
+    score = round(normality_scores.loc[feature]["W"], 3)
+    pval = round(normality_scores.loc[feature]["pval"], 3)
+    st.write(
+        f"{disp(feature)} scored {score} and has a p-value f {pval}. In "
+        "order to be considered normal, the p-value must be larger than "
+        "0.05."
+    )
+    st.write(
+        "While none of the features were found to be normally "
+        "distributed, we were able to transform some of them in order to "
+        "obtain normal distributions."
+    )
+    st.write("Select a feature to plot its distribution for home and away " "teams.")
+    single_features = [feature.split("_")[0] for feature in features]
     feature_dist = st.selectbox("Feature", single_features, index=0)
-    home_feat = feature_dist + '_home'
-    away_feat = feature_dist + '_away'
+    home_feat = feature_dist + "_home"
+    away_feat = feature_dist + "_away"
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 5))
-    home_score = round(normality_scores.loc[home_feat]['pval'],4)
-    away_score = round(normality_scores.loc[away_feat]['pval'],4)
+    home_score = round(normality_scores.loc[home_feat]["pval"], 4)
+    away_score = round(normality_scores.loc[away_feat]["pval"], 4)
     sns.histplot(data=data, x=home_feat, kde=True, hue="home_wins", ax=axes[0])
     axes[0].set_title(f"\n{disp(home_feat)} p-value = {home_score}")
     sns.histplot(data=data, x=away_feat, kde=True, hue="home_wins", ax=axes[1])
-    plt.title(f"{disp(feature_dist)} Distribution: Home vs. Away\n"\
-              f"{disp(away_feat)} p-value = {away_score}")
+    plt.title(
+        f"{disp(feature_dist)} Distribution: Home vs. Away\n"
+        f"{disp(away_feat)} p-value = {away_score}"
+    )
     st.pyplot(fig)
-    st.write("In order for the test to determine that the distribution is "\
-             "normal, we must have a p-value larger than 0.05.")
-    st.write("Notice that there is a certain symmetry when interchanging the "\
-            "roles home and away.")
-    
-    st.write('## Correlation (Spearman) and Predictive Power score\n')
-    st.write("Correlation coefficients are measures of the strength of the "\
-             "relationship between two random variables. It is a very widely "\
-             "used measurement.")
-    st.write("The Predictive Power score is an asymmetric statistic that "\
-             "can be used to analyze data. It can often find patterns that "\
-             "correlation coefficients miss.")
-    st.write("Select two features to compare. they will be plotted against "\
-             "each other in a scatter plot.")
+    st.write(
+        "In order for the test to determine that the distribution is "
+        "normal, we must have a p-value larger than 0.05."
+    )
+    st.write(
+        "Notice that there is a certain symmetry when interchanging the "
+        "roles home and away."
+    )
+
+    st.write("## Correlation (Spearman) and Predictive Power score\n")
+    st.write(
+        "Correlation coefficients are measures of the strength of the "
+        "relationship between two random variables. It is a very widely "
+        "used measurement."
+    )
+    st.write(
+        "The Predictive Power score is an asymmetric statistic that "
+        "can be used to analyze data. It can often find patterns that "
+        "correlation coefficients miss."
+    )
+    st.write(
+        "Select two features to compare. they will be plotted against "
+        "each other in a scatter plot."
+    )
     st.write("The features with the highest correlation coefficients are: \n")
-    home_wins_index = features.index('home_wins')
-    pm_index = features.index('plus_minus_home')
+    home_wins_index = features.index("home_wins")
+    pm_index = features.index("plus_minus_home")
     feature_1 = st.selectbox("Feature 1", features, index=home_wins_index)
     feature_2 = st.selectbox("Feature 2", features, index=pm_index)
-    fig, axes = plt.subplots(figsize=(7, 5)) 
+    fig, axes = plt.subplots(figsize=(7, 5))
     sns.scatterplot(data=data, x=feature_1, y=feature_2, ax=axes)
-    plt.title(f"{disp(feature_1)} vs. {disp(feature_2)}: "\
-              f"{corr_df[feature_1][feature_2]}")
+    plt.title(
+        f"{disp(feature_1)} vs. {disp(feature_2)}: " f"{corr_df[feature_1][feature_2]}"
+    )
     st.pyplot(fig)
-    ppscore = pps_results.query('x == @feature_1 & y == @feature_2')
-    ppscore = round(ppscore['ppscore'].iloc[0],4)
-    st.write("The respective Predictive Power scores are: \n"\
-             f"* {disp(feature_1)} influences {disp(feature_2)}: {ppscore}\n ")
-    
+    ppscore = pps_results.query("x == @feature_1 & y == @feature_2")
+    ppscore = round(ppscore["ppscore"].iloc[0], 4)
+    st.write(
+        "The respective Predictive Power scores are: \n"
+        f"* {disp(feature_1)} influences {disp(feature_2)}: {ppscore}\n "
+    )
+
     st.write("### Some interesting relationships:")
-    st.write("* Year and 3 point shots (made and attempted)")
-    st.write("* Aside from point related statistics, the features that had "\
-             "the highest correlation with home_wins are: \n"\
-             "  * ast_away\n  * ast_home\n  * dreb_away\n  * dreb_home\n"\
-             "  * fgm_away\n  * fgm_home")
-    st.write("* Only Plus/Minus Home and Pts Home have non-trivial predictive"\
-             " power scores with respect to Home Wins.")
-    
+    st.write("* Year and 3 point shots (made as well as attempted)")
+    st.write(
+        "* Aside from point related statistics, the features that had "
+        f"the highest correlation with {disp('home_wins')} are (in order): \n"
+        f"  * {disp('ast_away')}\n"
+        f"  * {disp('ast_home')}\n"
+        f"  * {disp('dreb_away')}\n"
+        f"  * {disp('dreb_home')}\n"
+    )
+    st.write(
+        "* Only Plus/Minus Home and Pts Home have non-trivial predictive"
+        " power scores with respect to Home Wins."
+    )
+    st.write("\n")
+    st.write("These relationships address Business Requirement 1 and "
+             "Hytpothesis 2.")
--- app_pages/page_hypothesis_1.py
+++ app_pages/page_hypothesis_1.py
@@ -8,15 +8,41 @@ import plotly.express as px
 
 def page_hypothesis_1_body():
     st.write("## Naive Feature Selection")
-    st.write("Our first hypothesis, is that if allowed, all of our "
-             "classification models will gravitate towards features that "
-             "relate to made shots. These are:"
+    st.write("Our first hypothesis relates to how models select features and "
+             "which features the different supervised learning algorithms "
+             "will prefer. The winner of a game of basketball is determined "
+             "by the team that scores more points."
+             )
+    st.write("\n")
+    st.info("* Hypothesis 1: \n"
+             "  * ML algorithms will gravitate towards the obvious statistics"
+             " (point related, such as made shots) in order to construct "
+             "their models.")
+    st.write("\n")
+    st.write("To validate this hypothesis, we looked trained various models "
+             "on our data and looked at which features it used. We considered"
+             " the following classification models:\n"
+             "* Logistic Regression\n"
+             "* Decision Tree Classifier\n"
+             "* Random Forest Classifier\n"
+             "* Gradient Boosting Classifier\n"
+             "* Extra Trees Classifier\n"
+             "* Adaptive Boost Classifier\n"
+             "* XGBoost Classifier\n")
+    st.write("\n")
+    st.write("Specifically, we attempted to validate Hypothesis 1 by doing "
+             "the following:")
+    st.info("Train the above models on all features and investigate which are"
+            " selected.")
+    st.write("\n")
+    st.write("The 'point related' statistics are the following:\n"
              "* `plus_minus_home (`plus_minus_away` was already removed)\n"
              "* `pts_home`, `pts_away`\n"
              "* `ftm_home`, `ftm_away`\n"
              "* `fgm_home`, `fgm_away` \n"
              "* `fg3m_home`, `fg3m_away` \n"
              "The first 2 rows are features from which the winner of the game "
-             "can be directly determined. We saw in our correlation study that."
+             "can be directly determined. We saw in our correlation study that"
+    
         "We did this to test Hypothesis 3: that a good pipeline can be made"
         " without these features.")
